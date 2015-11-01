d087567a69bd1cd1092d10a54bdc7953d8386016
Marcus Chang <marcus.chang@arm.com>
2015-11-01 17:51:47 +0000

    Updated test and dependencies.

--- test/main.cpp
+++ test/main.cpp
@@ -19,42 +19,49 @@
 #include "message-center/MessageCenter.h"
 #include "message-center-transport/MessageCenterSPIMaster.h"
 
-#include "core-util/SharedPointer.h"
 
 #if defined(TARGET_LIKE_EFM32GG_STK)
-const PinName SPI_MOSI = PD0;
-const PinName SPI_MISO = PD1;
-const PinName SPI_SCLK = PD2;
-const PinName SPI_SSEL = PD3;
-const PinName SPI_IRQ  = PD4;
-const PinName BUTTON1 = BTN0;
-const PinName BUTTON2 = BTN1;
+const PinName MC_SPI_MOSI = PD0;
+const PinName MC_SPI_MISO = PD1;
+const PinName MC_SPI_SCLK = PD2;
+const PinName MC_SPI_SSEL = PD3;
+const PinName MC_SPI_IRQ  = PD4;
+const PinName MC_BUTTON1 = BTN0;
+const PinName MC_BUTTON2 = BTN1;
 #elif defined(TARGET_LIKE_K64F)
-const PinName SPI_MOSI = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MOSI;
-const PinName SPI_MISO = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MISO;
-const PinName SPI_SCLK = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SCLK;
-const PinName SPI_SSEL = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SSEL;
-const PinName SPI_IRQ  = PTC4;
-const PinName BUTTON1 = SW2;
-const PinName BUTTON2 = SW3;
+const PinName MC_SPI_MOSI = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MOSI;
+const PinName MC_SPI_MISO = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MISO;
+const PinName MC_SPI_SCLK = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SCLK;
+const PinName MC_SPI_SSEL = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SSEL;
+const PinName MC_SPI_IRQ  = PTC4;
+const PinName MC_BUTTON1 = SW2;
+const PinName MC_BUTTON2 = SW3;
+#elif defined(TARGET_LIKE_COFFEE_ODIN)
+const PinName MC_SPI_MOSI = YOTTA_CFG_HARDWARE_PINS_SPI_MOSI;
+const PinName MC_SPI_MISO = YOTTA_CFG_HARDWARE_PINS_SPI_MISO;
+const PinName MC_SPI_SCLK = YOTTA_CFG_HARDWARE_PINS_SPI_SCLK;
+const PinName MC_SPI_SSEL = YOTTA_CFG_HARDWARE_PINS_SPI_SSEL;
+const PinName MC_SPI_IRQ  = YOTTA_CFG_HARDWARE_PINS_SPI_IRQ;
+const PinName MC_BUTTON1 = YOTTA_CFG_HARDWARE_PINS_BUTTON1;
+const PinName MC_BUTTON2 = YOTTA_CFG_HARDWARE_PINS_BUTTON2;
 #else
-const PinName SPI_MOSI = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MOSI;
-const PinName SPI_MISO = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MISO;
-const PinName SPI_SCLK = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SCLK;
-const PinName SPI_SSEL = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SSEL;
-const PinName SPI_IRQ  = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_IRQ;
+const PinName MC_SPI_MOSI = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MOSI;
+const PinName MC_SPI_MISO = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_MISO;
+const PinName MC_SPI_SCLK = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SCLK;
+const PinName MC_SPI_SSEL = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_SSEL;
+const PinName MC_SPI_IRQ  = YOTTA_CFG_HARDWARE_TEST_PINS_SPI_IRQ;
 #endif
 
-static SPI spi(SPI_MOSI, SPI_MISO, SPI_SCLK);
-static MessageCenterSPIMaster transport(spi, SPI_SSEL, SPI_IRQ);
+static SPI spi(MC_SPI_MOSI, MC_SPI_MISO, MC_SPI_SCLK);
+static MessageCenterSPIMaster transport(spi, MC_SPI_SSEL, MC_SPI_IRQ);
 
 static uint8_t buffer[100];
 static BlockStatic block(buffer, sizeof(buffer));
 
 
 // enable buttons to initiate transfer
-static InterruptIn button1(BUTTON1);
-static InterruptIn button2(BUTTON2);
+static InterruptIn button1(MC_BUTTON1);
+static InterruptIn button2(MC_BUTTON2);
 
 // liveness led
 static DigitalOut led1(LED1);
@@ -65,7 +72,7 @@ static Ticker ticker;
 
 void receivedBlock(BlockStatic block)
 {
-    printf("main:received: %p\r\n", block.getData());
+    printf("main: 0x0100: %p\r\n", block.getData());
     for (std::size_t idx = 0; idx < block.getLength(); idx++)
     {
         printf("%02X", block.at(idx));
@@ -92,7 +99,7 @@ void button1Task()
         block.at(idx) = idx;
     }
 
-    MessageCenter::sendTask(MessageCenter::RemoteHost, 1234, block, sendDone);
+    MessageCenter::sendTask(MessageCenter::RemoteHost, 0x0100, block, sendDone);
 }
 
 void button1ISR()
@@ -110,7 +117,7 @@ void button2Task()
         block.at(idx) = idx;
     }
 
-    MessageCenter::sendTask(MessageCenter::LocalHost, 1234, block, sendDone);
+    MessageCenter::sendTask(MessageCenter::LocalHost, 0x0100, block, sendDone);
 }
 
 void button2ISR()
@@ -136,7 +143,7 @@ void app_start(int, char *[])
 
     MessageCenter::addTransportTask(MessageCenter::RemoteHost, &transport);
 
-    MessageCenter::addListenerTask(MessageCenter::RemoteHost, 1234, receivedBlock);
+    MessageCenter::addListenerTask(MessageCenter::LocalHost, 0x0100, receivedBlock);
 
     printf("Message Center: %s %s\r\n", __DATE__, __TIME__);
 }
